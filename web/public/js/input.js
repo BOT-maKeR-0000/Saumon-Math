6ffe52c3c5b010754b3013449e34661768b03191
Bot-maker <leys.kamil@gmail.com>
2022-10-29 21:35:49 +0200

    small error fix

--- web/public/js/input.js
+++ web/public/js/input.js
@@ -3,6 +3,6 @@ window.onload(() => {
     inputs.forEach((input) => {
         input.addEventListener('input', (e) => {
             e.value = e.value.replace(/[^\d]/g, '');
-        }
+        });
     });
 });
